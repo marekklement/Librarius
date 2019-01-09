25614126e9e61f9d1cc136849f4e00563553b02b
Petr Kostka <kostkpe3@fel.cvut.cz>
2019-01-09 02:16:55 +0100

    Minor changes.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,6 +9,9 @@ INSERT INTO LI_BOOK("id_book", "language", "title") VALUES
    (1000, 'CZ', 'JajaAPaja'),
    (2000, 'SK', 'KrtecekAKalhotky');
 
+INSERT INTO LI_BOOK_AUTH("id_book","id_author") VALUES
+    (1000, 1000),
+    (2000, 2000);
 INSERT INTO LI_LISTING("id_listing", "isbn", "price", "creation_time", "auto_graphed", "id_book") VALUES
    (1000, 1234, 100, to_date('1963-09-01', 'YYYY-MM-DD'), false, 1000),
    (2000, 9876, 99, to_date('1963-09-01', 'YYYY-MM-DD'), false, 2000);
